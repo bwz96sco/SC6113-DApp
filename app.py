3f8744ce39608eb0e12cc1b7db2b50c1866b2510
ZhangBowen <bwz96sco@outlook.com>
2024-09-05 21:14:30 +0800

    database related

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask,render_template,request
+from flask import Flask, render_template, request
+import sqlite3
+import datetime
 
 app=Flask(__name__)
 
@@ -9,6 +11,13 @@ def index():
 @app.route('/main', methods = ["get", "post"])
 def main():
     r = request.form.get('q')
+    current_time = datetime.datetime.now()
+    conn = sqlite3.connect('dapp.db')
+    c = conn.cursor()
+    c.execute('INSERT INTO user (name, timestamp) VALUES (?, ?)', (r, current_time))
+    conn.commit()
+    c.close()
+    conn.close()
     return render_template('main.html', r=r)
 
 @app.route('/store_money', methods = ["get", "post"])
@@ -18,6 +27,32 @@ def store_money():
 @app.route('/transfer_money', methods = ["get", "post"])
 def transfer_money():
     return render_template('transfer_money.html')
+
+@app.route('/admin', methods = ["get", "post"])
+def admin():
+    return render_template('admin.html')
+
+@app.route('/viewDB', methods = ["get", "post"])
+def viewDB():
+    conn = sqlite3.connect('dapp.db')
+    c = conn.cursor()
+    c.execute('SELECT * FROM user')
+    r = ""
+    for row in c.fetchall():
+        r += str(row) + "\n"
+    c.close()
+    conn.close()
+    return render_template('viewDB.html', r = r)
+
+@app.route('/delDB', methods = ["get", "post"])
+def delDB():
+    conn = sqlite3.connect('dapp.db')
+    c = conn.cursor()
+    c.execute('DELETE FROM user')
+    conn.commit()
+    c.close()
+    conn.close()
+    return render_template('delDB.html')
 
 if __name__=='__main__':
     app.run()
